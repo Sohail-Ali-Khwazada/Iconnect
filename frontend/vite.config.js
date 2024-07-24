0c6b88d6d731b5e706a5ded8ae3d467ca2e4af42
Sohail Ali Khwazada <sohailali.khwazada2011@gmail.com>
2024-07-25 00:18:55 +0530

    Added proper urls for frontend and backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy:{
-      "/api":"http://localhost:5000",
+      "/api":"https://my-chat-app-6xac.onrender.com",
     }
   }
 })
