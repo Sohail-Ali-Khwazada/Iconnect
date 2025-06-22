1f33abf07703c215d60cf47cc05746ed2b90c357
Sohail Ali Khwazada <sohailali.khwazada2011@gmail.com>
2025-06-22 16:56:35 +0530

    Added the ui for typing indicator (full feature done)

--- frontend/src/components/MessageDisplay/Messages.jsx
+++ frontend/src/components/MessageDisplay/Messages.jsx
@@ -1,23 +1,21 @@
 import { useEffect, useRef } from "react";
-import useGetMessages from "../../hooks/useGetMessages"
-import Message from "./Message"
+import useGetMessages from "../../hooks/useGetMessages";
+import Message from "./Message";
 import useListenMessages from "../../hooks/useListenMessages";
 
-
-function Messages() {
+function Messages({ typingStatus }) {
   const { messages, loading } = useGetMessages();
   useListenMessages();
   const lastMessageRef = useRef();
 
-
-  useEffect(()=>{
+  useEffect(() => {
     setTimeout(() => {
-      lastMessageRef.current?.scrollIntoView({behavior: "smooth"});
-    },100)
-  },[messages])
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages,typingStatus]);
 
   return (
-    <div className="overflow-y-auto px-4 h-[83%] ">
+    <div className="overflow-y-auto px-4 h-[83%]">
       {loading && (
         <>
           <MessageSkeleton />
@@ -26,40 +24,50 @@ function Messages() {
         </>
       )}
       {!loading && messages.length === 0 && (
-        <p className="text-center text-white">Send a message to start the conversation</p>
+        <p className="text-center text-white">
+          Send a message to start the conversation
+        </p>
       )}
 
-      {!loading && messages.length > 0 && messages.map((message) => (
-        <div key={message._id} ref={lastMessageRef}>
-        <Message message={message} />
-        </div>
-      ))}
-    </div>
-  )
-}
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
 
+      {typingStatus && (
+        <div className="chat chat-start px-7 py-1">
+          <div className="chat-bubble flex gap-1 items-center">
+            <span className="w-2 h-2 bg-[#6D3DF2] rounded-full animate-bounce [animation-delay:0s]"></span>
+            <span className="w-2 h-2 bg-[#6D3DF2] rounded-full animate-bounce [animation-delay:0.2s]"></span>
+            <span className="w-2 h-2 bg-[#6D3DF2] rounded-full animate-bounce [animation-delay:0.4s]"></span>
+          </div>
+        </div>
+      )}
+
+      <div ref={lastMessageRef}></div>
+    </div>
+  );
+}
 
 function MessageSkeleton() {
   return (
     <>
-      <div className='flex gap-3 items-center'>
-        <div className='skeleton w-10 h-10 rounded-full shrink-0 border border-gray-700'></div>
-        <div className='flex flex-col gap-1'>
-          <div className='skeleton h-4 w-40 border border-gray-700'></div>
-          <div className='skeleton h-4 w-40 border border-gray-700'></div>
+      <div className="flex gap-3 items-center">
+        <div className="skeleton w-10 h-10 rounded-full shrink-0 border border-gray-700"></div>
+        <div className="flex flex-col gap-1">
+          <div className="skeleton h-4 w-40 border border-gray-700"></div>
+          <div className="skeleton h-4 w-40 border border-gray-700"></div>
         </div>
       </div>
-      <div className='flex gap-3 items-center justify-end'>
-        <div className='flex flex-col gap-1'>
-          <div className='skeleton h-4 w-40 border border-gray-700'></div>
+      <div className="flex gap-3 items-center justify-end">
+        <div className="flex flex-col gap-1">
+          <div className="skeleton h-4 w-40 border border-gray-700"></div>
         </div>
-        <div className='skeleton w-10 h-10 rounded-full shrink-0 border border-gray-700'></div>
+        <div className="skeleton w-10 h-10 rounded-full shrink-0 border border-gray-700"></div>
       </div>
     </>
   );
-};
+}
 
-
-export default Messages
-
-
+export default Messages;
--- frontend/src/components/MessageDisplay/index.jsx
+++ frontend/src/components/MessageDisplay/index.jsx
@@ -1,76 +1,82 @@
 import Messages from "./Messages";
 import Messageinput from "./Messageinput";
 import { TiMessages } from "react-icons/ti";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { useGlobalContext } from "../../context/GlobalContext";
 import { useSocketContext } from "../../context/SocketContext";
 
-
-
 function MessageDisplay() {
-  const {selectedConversation, setSelectedConversation} = useGlobalContext();
-  const {socket} = useSocketContext();
+  const { selectedConversation, setSelectedConversation } = useGlobalContext();
+  const { socket } = useSocketContext();
   const [typingStatus, setTypingStatus] = useState(false);
 
+  useEffect(() => {
+    socket?.on("typing", ({ from }) => {
+      if (from === selectedConversation?._id) {
+        setTypingStatus(true);
+      }
+    });
 
+    socket?.on("stopTyping", ({ from }) => {
+      if (from === selectedConversation?._id) {
+        setTypingStatus(false);
+      }
+    });
 
-  useEffect(()=> {
-  socket?.on("typing", ({ from }) => {
-    if (from === selectedConversation?._id) {
-      setTypingStatus(true);
-      console.log("hello");
-    }
-  });
-
-  socket?.on("stopTyping", ({ from }) => {
-    if (from === selectedConversation?._id) {
-
-      setTypingStatus(false);
-      console.log("hello off");
-    }
-  });
-
-    return ()=> {
+    return () => {
       socket?.off("typing");
       socket?.off("stopTyping");
       // setSelectedConversation(null);
-    }
-  },[selectedConversation]);
+    };
+  }, [selectedConversation]);
 
   return (
-    <div className={`md:flex md:flex-col md:w-3/4 border border-black w-screen ${!selectedConversation && "hidden"}`}>
-      {!selectedConversation ? <NoChatSelected /> :<><div className="bg-[#444C51] px-4 py-4 mb-2 flex items-center relative">
-        <span className="label-text w-7 rounded-full">          
-            <img
-              alt="Profile photo"
-              src={selectedConversation.profilePic} />
-          </span>
-        <span className="font-bold ml-2">{selectedConversation.fullName}</span>
-        <span className="font-bold ml-2">{typingStatus && "Typing..."}</span>
-        <IoMdArrowRoundBack className="absolute right-3 w-8 h-5 cursor-pointer" onClick={()=> setSelectedConversation(null)}/>
-      </div>
-        <Messages />
-        <Messageinput />
-      </>
-      }
+    <div
+      className={`md:flex md:flex-col md:w-3/4 border border-black w-screen ${
+        !selectedConversation && "hidden"
+      }`}
+    >
+      {!selectedConversation ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          <div className="bg-[#444C51] px-4 py-4 mb-2 flex items-center relative">
+            <span className="label-text w-8 rounded-full">
+              <img
+                className="rounded-full"
+                alt="Profile photo"
+                src={selectedConversation.profilePic}
+              />
+            </span>
+            <span className="font-bold ml-2">
+              {selectedConversation.fullName}
+            </span>
+            <IoMdArrowRoundBack
+              className="absolute right-3 w-8 h-5 cursor-pointer"
+              onClick={() => setSelectedConversation(null)}
+            />
+          </div>
+
+          <Messages typingStatus={typingStatus}/>
+          <Messageinput />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 function NoChatSelected() {
-  const {authUser} = useGlobalContext();
+  const { authUser } = useGlobalContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl font-semibold flex flex-col items-center gap-2 text-gray-200">
         <p>Welcome 👋 {authUser.fullName}</p>
         <p>Select a chat to start messaging</p>
-        <TiMessages className="text-3xl md:text-6xl text-center"/>
+        <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
     </div>
   );
 }
 
 export default MessageDisplay;
-
-
